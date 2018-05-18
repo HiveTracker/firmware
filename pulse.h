7ed801283a660bb1e494b9c7e64b878722c285f0
Cedric Honnet <honnet@telecom-paristech.org>
2018-05-18 18:10:03 +0200

    WIP: Refactoring.

--- pulse.h
+++ pulse.h
@@ -16,12 +16,12 @@ const int forkTimer = 4;
 
 const int timerNumbers[] = {syncTimer, forkTimer};
 
-const int microSecToTicks = 16; // because 16MHz
+const int ticksPerMicrosec = 16; // because 16MHz
 
 const int margin = 10;          // microsec
-const int minSyncPulseWidth = (62.5 - margin) * microSecToTicks;
-const int maxSyncPulseWidth = (135  + margin) * microSecToTicks;
-const int skipThreshold = (104 + ceil(93.8)) / 2;
+const int minSyncPulseWidth = (62.5 - margin) * ticksPerMicrosec;
+const int maxSyncPulseWidth = (135  + margin) * ticksPerMicrosec;
+const int skipThreshold = ((104 + ceil(93.8)) / 2) * ticksPerMicrosec;
 
 const int interSyncOffset = 400; // microsec
 
@@ -37,8 +37,8 @@ const int interSyncOffset = 400; // microsec
     k3	1	1	1	6500	135
     */
 
-const int minSweepPulseWidth = 1  * microSecToTicks;
-const int maxSweepPulseWidth = 15 * microSecToTicks;
+const int minSweepPulseWidth = 1  * ticksPerMicrosec;
+const int maxSweepPulseWidth = 15 * ticksPerMicrosec;
 
 const int sweepEndTime = 6777 + margin; // microsec
 

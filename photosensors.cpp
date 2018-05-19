ee799db0eef10dbc8b1ed92454fcd42b83fdedca
Cedric Honnet <honnet@telecom-paristech.org>
2018-05-19 14:38:29 +0200

    WIP: Allow retrying TS4231 init as much as possible.

--- photosensors.cpp
+++ photosensors.cpp
@@ -12,40 +12,70 @@ TS4231 devices[] = { TS4231(sensors_e[0], sensors_d[0]),
                      TS4231(sensors_e[3], sensors_d[3]) };
 
 void photosensorSetup() {
+    uint8_t config_result = -1;
+
+    pinMode(LED_G, OUTPUT);
+    pinMode(LED_B, OUTPUT);
+
     for (int i = 0; i < sensors_num; i++) {
-        if (devices[i].waitForLight(light_timeout)) {
+        DEBUG_PRINTLN(i);
+        bool exit = 0;
+
+        while (!exit) {
+            if (!devices[i].waitForLight(light_timeout)) {
+                DEBUG_PRINTLN("TIMEOUT");
+                continue;
+            }
+
             DEBUG_PRINTLN("LD"); // Light Detected
 
-            uint8_t config_result = devices[i].configDevice();
+            config_result = devices[i].configDevice();
 
             switch (config_result) {
                 case CONFIG_PASS:
+                    exit = 1;
                     DEBUG_PRINTLN("OK");
                     break;
                 case BUS_FAIL:
                     // unable to resolve state of TS4231 (3 samples of the bus
                     // signals resulted in 3 different states)
+                    exit = 1;
                     DEBUG_PRINTLN("ERROR: BUS_FAIL");
                     break;
                 case VERIFY_FAIL:
                     // configuration read value did not match configuration write
                     // value, run configuration again
+                    exit = 0;
+                    digitalWrite(LED_B, digitalRead(LED_B)); // toggle
                     DEBUG_PRINTLN("ERROR: VERIFY_FAIL");
                     break;
                 case WATCH_FAIL:
                     // verify succeeded but entry into WATCH mode failed, run
                     // configuration again
+                    exit = 0;
+                    digitalWrite(LED_G, digitalRead(LED_G)); // toggle
                     DEBUG_PRINTLN("ERROR: WATCH_FAIL");
                     break;
                 default:  //value returned was unknown
+                    exit = 1;
                     DEBUG_PRINTLN("ERROR: unknown value");
                     break;
             }
         }
-        else {
-            //insert code here for no light detection
-            DEBUG_PRINTLN("TIMEOUT");
+    }
+
+    if (config_result != CONFIG_PASS) {
+        while(1) {
+            digitalWrite(LED_B, digitalRead(LED_B)); // toggle
+            digitalWrite(LED_G, digitalRead(LED_G)); // toggle
+            DEBUG_PRINTLN("BUS_FAIL (or unknown error)");
+            delay(500);
         }
     }
+
+    // Turn of LED warnings (inverted logic)
+    digitalWrite(LED_G, 1);
+    digitalWrite(LED_B, 1);
+    DEBUG_PRINTLN("EVERYTHING OK");
 }
 

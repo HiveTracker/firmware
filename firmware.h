c80595d68a3ee3f07e362a8f521b26ddb4e1138c
Cedric Honnet <honnet@telecom-paristech.org>
2018-08-20 16:25:39 +0200

    Allow multiple args in DEBUG_PRINT().

--- firmware.h
+++ firmware.h
@@ -2,14 +2,14 @@
 #define _FIRMWARE_H_
 
 #define DEBUG
+
 #ifdef DEBUG
 #include "Arduino.h"
-#define DEBUG_PRINT(x)     Serial.print(x)
-#define DEBUG_PRINTLN(x)   Serial.println(x)
+#define DEBUG_PRINT(...)     Serial.print(__VA_ARGS__)
+#define DEBUG_PRINTLN(...)   Serial.println(__VA_ARGS__)
 #else
-#define DEBUG_PRINT(x)
-#define DEBUG_PRINTLN(x)
+#define DEBUG_PRINT(...)
+#define DEBUG_PRINTLN(...)
 #endif
 
 #endif // _FIRMWARE_H_
-

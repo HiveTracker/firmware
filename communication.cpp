09e9349a8a8c2e4bed960e83837f2a308030453e
Cedric Honnet <honnet@telecom-paristech.org>
2019-09-29 02:13:52 +0200

    WIP (only compiled): stream simulation binary file.

--- communication.cpp
+++ communication.cpp
@@ -2,10 +2,15 @@
 #include <BLEPeripheral.h>
 #include "BLESerial.h"
 
-#include "pulse.h"
 #include "pinout.h" // important to keep it last for the undefs to work
 #include "communication.h"
 #include "firmware.h"
+
+#include "simulation.h" // contains raw data recorded from simulation app
+// it's generated with: xxd -i data.bin > simulation.h
+// this command creates data_bin[] and data_bin_len
+// max data size is about 370KB
+
 
 byte message[10];
 BLESerial bleSerial;
@@ -24,8 +29,6 @@ void pinsSetup()
         delay(100);
         digitalWrite(LEDpins[i], 1); // 1 = OFF (inverted logic)
     }
-
-    pinMode(PIN_SERIAL_RX, OUTPUT); // TODO REMOVE (TMP)
 }
 
 void serialSetup()
@@ -46,45 +49,16 @@ void wirelessSetup()
 
 void sendPulseData()
 {
-    // Send binary metadata (base ID and axis)
-    uint8_t base_axis = pulse_data.baseID << 1 | pulse_data.axis;
-    message[0] = base_axis;
-    message[1] = 0;
-    for (int t = 0, i = 0; t < 2; t++)
-    {
-        // send captures
-        for (int c = 0; c < sensors_num; c += 2, i++)
-        {
-            int pulseStart = pulse_data.sweep_captures[t][c];
-            int pulseEnd = pulse_data.sweep_captures[t][c + 1];
-            int pulseWidthTicks16 = pulseEnd - pulseStart;
-            // TODO: deport data verification to higher level?
-            if (pulseWidthTicks16 < minSweepPulseWidth || // ticks
-                pulseWidthTicks16 > maxSweepPulseWidth || // ticks
-                pulseStart < sweepStartTicks ||           // ticks
-                pulseEnd > sweepEndTicks)                 // ticks
-            {
-                // mark the measures if they are invalid
-                pulseStart = 0;
-                pulseEnd = 0;
-            }
+    static int offset = 0;
 
-            // get centroid + remove 2 LSb (non-significant) to stay in 16bit
-            // TODO: use differential loss-less compression
-            int centroid = ((pulseEnd + pulseStart) / 2) >> 2;
-            message[i * 2 + 2] = (centroid >> 8) & 0xFF; // MSB first
-            message[i * 2 + 3] = (centroid >> 0) & 0xFF; // LSB last
-            message[1] += centroid & 0xFF;               // add LSB to checksum
-        }
-    }
-
-    // set the high-bits and metadata on message separator (base/axis + checksum)
-    message[0] = 0x80 | (message[0] << 5) & 0x60 | (message[1] >> 4) & 0x0F;
-    message[1] = 0x80 | (message[1] >> 0) & 0x0F;
     if (bleSerial) {
-        bleSerial.write(message, 10);
         bleSerial.poll();
+        int bytesSent = bleSerial.write(data_bin + offset, 10);
+
+        offset += bytesSent;
+        if (offset + 10 >= data_bin_len) // anticipate next 10 bytes message
+            offset = 0;
     }
-    if (Serial)
-        Serial.write(message, 10);
+
+    delayMicroseconds(8333); // 120Hz
 }
--- firmware.ino
+++ firmware.ino
@@ -1,29 +1,12 @@
-#include "pinout.h"
 #include "communication.h"
-#include "pulse.h"
-
-
-void sendData() {
-    sendPulseData();
-    // sendOrientations();  // TODO
-    // sendAccelerations(); // TODO
-}
 
 void setup() {
     pinsSetup();
     serialSetup();
-
-    // Start pulse measurements and set data TX callback function (using timers)
-    pulseSetup(NULL); // NULL => use pulseDataIsReady() to know when ready
-
-    // IMUsetup(); // TODO
-
     wirelessSetup(); // IMPORTANT: KEEP IT LAST!
 }
 
 void loop() {
-    if (pulseDataIsReady()) { // should be called as often as possible
-        sendData();
-    }
+    sendPulseData(); // simulated
 }
 
